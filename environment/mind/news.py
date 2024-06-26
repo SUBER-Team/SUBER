8a36ddd2a6318ebf7bb5f927b881b28de108ea83
Anthony Sheller <sheller@ncsu-las.net>
2024-06-26 17:50:04 +0000

    First go at config, news and newsloader for MIND

--- environment/mind/news.py
+++ environment/mind/news.py
@@ -1,13 +1,16 @@
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
+from typing import List, Optional
 from environment.item import Item
 import pandas as pd
 
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer('sentence-transformers/sentence-t5-base')
+
 # Define data classes
 @dataclass
-class NewsArticle(Item):
+class News(Item):
     '''
-    Data structure for a newsArticle
+    Data structure for a newsArticle.  Decided to call it news cause that's shorter.
     '''
     news_id: str
     category: str
@@ -17,16 +20,16 @@ class NewsArticle(Item):
     url: str
     title_entities: str
     abstract_entities: str
+    _title_embeddings: Optional[List[float]] = field(default=None, repr=False, init=False)
+    _abstract_embeddings: Optional[List[float]] = field(default=None, repr=False, init=False)
 
-   def __init__(
+    def __init__(
         self,
         news_id: str,
         category: str,
         subcategory: str,
         title: str,
-        title_embeddings: List[float],
         abstract: str,
-        abstract_embeddings: List[float],
         url: str,
         title_entities: str,
         abstract_entities: str,
@@ -37,26 +40,34 @@ class NewsArticle(Item):
         self.category = category
         self.subcategory = subcategory
         self.title = title
-        self.title_embeddings = title_embeddings
         self.abstract = abstract
-        self.abstract_embeddings = abstract_embeddings
         self.url = url
         self.title_entities = title_entities
         self.abstract_entities = abstract_entities
 
+    @property
+    def title_embeddings(self):
+        if self._title_embeddings is None:
+            self._title_embeddings = model.encode(self.title).tolist()
+        return self._title_embeddings
+
+    @property
+    def abstract_embeddings(self):
+        if self._abstract_embeddings is None:
+            self._abstract_embeddings = model.encode(self.abstract).tolist()
+        return self._abstract_embeddings
+
     @staticmethod
     def from_dataframe(df: pd.DataFrame):
-        return NewsArticle(
+        return News(
             news_id = df["news_id"],
             category = df["category"],
             subcategory = df["subcategory"],
             title  = df["title"],
-            title_embeddings  = eval(df["title_embeddings"]),
             abstract = df["abstract"],
-            abstract_embeddings = eval(df["abstract_embeddings"]),
             url = df["url"],
-            title_entities = df["title-entities"],
-            abstract_entities = df["title-entities"],
+            title_entities = df["title_entities"],
+            abstract_entities = df["abstract_entities"],
         )
 
 
@@ -72,3 +83,30 @@ class UserBehavior(Item):
     time: str
     history: List[str]
     impressions: List[str]
+
+
+    def __init__(
+        self,
+        impression_id: str,
+        user_id: str,
+        time: str,
+        history: list[str],
+        impressions: List[str],        
+    ):
+        super().__init__(id, title)
+        self.impression_id = impression_id
+        self.user_id = user_id
+        self.time = time
+        self.history = history
+        self.impressions = impressions
+
+    @staticmethod
+    def from_dataframe(df: pd.DataFrame):
+        return NewsArticle(
+            impression_id = df["impression_id"],
+            user_id = df["user_id"],
+            time = df["time"],
+            history  = df["history"],
+            impressions = df["impressions"],
+        )
+
--- environment/mind/news_loader.py
+++ environment/mind/news_loader.py
@@ -1,1 +1,58 @@
-from .mind import 
+import os
+import json
+from environment.item import ItemsLoader
+from environment.mind.news import News
+from environment.mind.datasets.load_mind_data import load_data, news_columns,train_path, save_catagories_to_csv
+
+
+
+
+class NewsLoader(ItemsLoader):
+    '''
+    Responsible for loading news articles
+    '''
+
+    def __init__(self):
+        '''
+        Similar to movies we want to pass in data.  Movies does it with a Json file
+        We can do it with a dataframe to make it more efficient. IMHO.
+
+        do something different if it's necessary but this is easiest IMHO
+
+        '''
+        self.data = load_data(os.path.join(train_path, 'news.tsv'),news_columns)
+        self.data.index = self.data["news_id"]
+        save_catagories_to_csv(self.data)
+
+
+    def load_all_ids(self):
+        '''
+        define this abstract method
+        Return a list of integers containing the ids of all movies
+        '''
+        return self.data.index.tolist()
+
+    def load_items(self):
+        '''
+        Define the abstract method like books/movies
+        returns:
+            news (dictionary from news ids to news): a mapping for all 
+        '''
+        news = {}
+        for article in self.data:
+            news[article.index] = News.from_dataframe(article)
+        return news
+    
+    def load_items_from_ids(self,id_list):
+        '''
+        Define the abstract method
+        Returns a list of news articles
+        '''
+        news = []
+        for id in id_list:
+            print("ID is {} and type is ".format(id,type(id)))
+            print("\t{}\t\n".format(self.data.loc[id]))
+            news.append(News.from_dataframe(self.data.loc[id]))
+        return news
+
+        
